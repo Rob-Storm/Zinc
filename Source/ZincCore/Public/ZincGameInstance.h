bb6cdc789a0176cf205488bc8a753351dc4f238d
The1Wolfcast <the1wolfcast@gmail.com>
2026-05-28 16:59:29 -0500

    audio settings done

--- Source/ZincCore/Public/ZincGameInstance.h
+++ Source/ZincCore/Public/ZincGameInstance.h
@@ -14,7 +14,22 @@ public:
 
 	virtual void Init() override;
 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Input")
+	float LastLookSensitivity = 1.0f;
+
 	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="GameFlow")
 	bool StartCutsceneTriggered = false;
+
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Volume")
+	TMap<USoundClass*, float> SoundClassVolumes;
+
+	UFUNCTION(BlueprintImplementableEvent, BlueprintCallable, Category="Volume")
+	void SetClassVolume(USoundClass* SoundClass, float Volume);
+
+	UFUNCTION(BlueprintPure, BlueprintCallable, Category="Volume")
+	float GetClassVolume(USoundClass* SoundClass) const
+	{
+		return SoundClassVolumes.FindRef(SoundClass);
+	}
 	
 };
